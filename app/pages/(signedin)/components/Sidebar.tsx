231bca0dc3e9bc90d8ed6daf006791efa7d7bd5c
Nky237 <ezikenkemdilijane@gmail.com>
2024-05-23 02:31:23 +0100

    did the navbar

--- app/pages/(signedin)/components/Sidebar.tsx
+++ app/pages/(signedin)/components/Sidebar.tsx
@@ -1,73 +1,144 @@
 "use client";
 import React, { useState } from "react";
-import { GrHomeRounded } from "react-icons/gr";
-import { IoChatboxEllipsesOutline } from "react-icons/io5";
-import Link from "next/link";
-import Logo from "@/assets/multiplify-logo.svg";
-import { useRouter } from "next/navigation";
 import Image from "next/image";
-const SideBar = () => {
-  const navList = [
-    {
-      name: "Dashboard",
-      icon: <GrHomeRounded />,
-      link: "/pages/dashboard",
-    },
-    {
-      name: "View profile",
-      icon: <IoChatboxEllipsesOutline />,
-      link: "/pages/profile",
-    },
-    {
-      name: "Media",
-      icon: <GrHomeRounded />,
-      link: "/pages/media",
-    },
-    {
-      name: "Properties",
-      icon: <IoChatboxEllipsesOutline />,
-      link: "/pages/property",
-    },
-    {
-      name: "Pages",
-      icon: <IoChatboxEllipsesOutline />,
-      link: "/pages/pages",
-    },
-    {
-      name: "Your Profile",
-      icon: <IoChatboxEllipsesOutline />,
-      link: "/pages/my-profile",
-    },
-  ];
-  const [activeIdx, setActiveIdx] = useState(0);
+import { usePathname } from "next/navigation";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { Disclosure } from "@headlessui/react";
+import {
+  MdOutlineSpaceDashboard,
+  MdOutlineAnalytics,
+  MdOutlineIntegrationInstructions,
+  MdOutlineMoreHoriz,
+  MdOutlineSettings,
+  MdOutlineLogout,
+} from "react-icons/md";
+import Link from "next/link";
+import Logo from "@/app/assets/landingLogo.svg";
 
-  const handleMenuItemClick = (idx: any) => {
-    setActiveIdx(idx);
+const navList = [
+  {
+    name: "Dashboard",
+    icon: <MdOutlineSpaceDashboard />,
+    link: "/pages/dashboard",
+  },
+  {
+    name: "View profile",
+    icon: <MdOutlineMoreHoriz />,
+    link: "/pages/profile",
+  },
+  {
+    name: "Media",
+    icon: <MdOutlineSettings />,
+    link: "/pages/media",
+  },
+  {
+    name: "Properties",
+    icon: <MdOutlineIntegrationInstructions />,
+    link: "/pages/property",
+  },
+  {
+    name: "Pages",
+    icon: <MdOutlineAnalytics />,
+    link: "/pages/pages",
+  },
+  {
+    name: "Your Profile",
+    icon: <MdOutlineIntegrationInstructions />,
+    link: "/pages/my-profile",
+  },
+];
+function SideNavbar() {
+  const [activeLink, setActiveLink] = useState("");
+  const pathname = usePathname();
+
+  const onLogOut = () => {
+    // Handle logout
   };
 
   return (
-    <div className="border-r-gray-light min-h-screen w-full  border-2 bg-[#023C63]  pt-7 text-white ">
-      {/* <div className="pb-7 pt-7">
-        <Image src={Logo} alt="" />
-      </div> */}
-      <ul className="flex h-screen flex-col  justify-around">
-        {navList.map((menu, idx) => (
-          <div
-            key={idx}
-            onClick={() => handleMenuItemClick(idx)}
-            className={`cursor-pointer ${idx === activeIdx ? "bg-[#D9D9D933]  text-white" : ""} rounded-xl py-5 `}
-          >
-            <div className="flex  items-center gap-4 pl-3">
-              <p>{menu.icon}</p>
-              <Link href={menu.link}>
-                <li>{menu.name}</li>
-              </Link>
+    <div>
+      <Disclosure as="nav">
+        {({ open, close }) => (
+          <>
+            <div className="fixed z-10 h-[60px] w-full bg-white lg:hidden">
+              <Disclosure.Button className="group peer  absolute right-4 top-4 inline-flex items-center justify-center rounded-md p-2 text-gray-800 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white lg:hidden">
+                <GiHamburgerMenu className="block h-6 w-6" aria-hidden="true" />
+              </Disclosure.Button>
             </div>
-          </div>
-        ))}
-      </ul>
+
+            {/* SMALL SCREEN */}
+            <div
+              className={`fixed inset-0 z-20 bg-black bg-opacity-50 transition-all duration-200 ease-out ${
+                open ? "block" : "hidden"
+              } lg:hidden`}
+            >
+              <div className="w-7/10 fixed left-0 top-0 h-screen bg-white p-6">
+                <div className="flex flex-col justify-start">
+                  {/* <div className="pb-7 pt-7">
+                    <Image src={Logo} alt="Logo" />
+                  </div> */}
+                  <div className="my-4 flex flex-col gap-y-8 border-b border-gray-100">
+                    {navList.map((menu, idx) => (
+                      <Link
+                        key={idx}
+                        href={menu.link}
+                        onClick={(e) => {
+                          setActiveLink(menu.link);
+                          close();
+                        }}
+                      >
+                        <div
+                          className={`group m-auto mb-2 flex cursor-pointer items-center justify-start gap-4 rounded-md p-2 pl-5 ${
+                            pathname === menu.link || activeLink === menu.link
+                              ? " bg-[#023C63] text-white shadow-lg"
+                              : "hover:[#023C63] text-gray-600 hover:text-white hover:shadow-lg"
+                          }`}
+                        >
+                          <p>{menu.icon}</p>
+                          <h3>{menu.name}</h3>
+                        </div>
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* LARGE SCREEN */}
+            <div className="hidden h-screen overflow-auto bg-[#023C63] lg:flex lg:w-60 lg:flex-col">
+              <div className="flex flex-col justify-start">
+                {/* <div className="pb-7 pt-7">
+                  <Image src={Logo} alt="" />
+                </div> */}
+                <div className="my-4 flex flex-col gap-y-8 border-b border-gray-100">
+                  {navList.map((menu, idx) => (
+                    <Link
+                      key={idx}
+                      href={menu.link}
+                      onClick={(e) => {
+                        setActiveLink(menu.link);
+                      }}
+                    >
+                      <div
+                        className={`group m-auto mb-2 flex cursor-pointer items-center justify-start gap-4 rounded-md p-2 pl-5 ${
+                          pathname === menu.link || activeLink === menu.link
+                            ? "bg-white text-gray-600 shadow-lg"
+                            : "text-white hover:bg-white hover:text-gray-600 hover:shadow-lg"
+                        }`}
+                      >
+                        <p>{menu.icon}</p>
+                        <h3>{menu.name}</h3>
+                      </div>
+                    </Link>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+      </Disclosure>
     </div>
   );
-};
+}
 
-export default SideBar;
+export default SideNavbar;
